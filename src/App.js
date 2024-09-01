52da625df05ff9cf3b11c16f9d37ded2f557898d
여민서 <yeominseo@yeominseoui-MacBookAir.local>
2024-09-01 23:45:29 +0900

    [2/n] Create TodoItem react component: Strikethrough todo item if marked finished

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+//Todo 아이템 버튼 눌릴때 스태이트에 추가하는 callback function 만들어서 props 로 패스하기
+let todoItemId = 0;
+
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
   console.log(input);
 
   //버튼 눌렸을 때 onSubmit callback 콜 해주기
-  const onSubmit = (newtodoitem) => {
+  const onSubmit = () => {
     props.onSubmit(input);
     setInput("");
   };
@@ -31,11 +34,31 @@ const TodoItemInputField = (props) => {
   );
 };
 
+// 등록된 todo 아이템을 위한 todoitem 리엑트 컴포넌트 만들기
+const TodoItem = (props) => {
+  const style = props.todoItem.isFinished
+    ? { textDecoration: "line-through" }
+    : {};
+  return (
+    <li>
+      <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>
+        {props.todoItem.todoItemContent}
+      </span>
+    </li>
+  );
+};
+
 //등록된 Todo 아이템 보여줄 TodoItemList 리엑트 콤포넌트 만들기
 const TodoItemList = (props) => {
   //새로운 투두아이템들 보여줘야함. props 통해 받아와야해서 todolist 형태도 받아온다
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <li key={index}>{todoItem.todoItemContent}</li>;
+    return (
+      <TodoItem
+        key={index}
+        todoItem={todoItem}
+        onTodoItemClick={props.onTodoItemClick}
+      />
+    );
   });
 
   return (
@@ -48,11 +71,40 @@ const TodoItemList = (props) => {
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([
+      ...todoItemList,
+      {
+        id: todoItemId++,
+        todoItemContent: newTodoItem,
+        isFinished: false,
+      },
+    ]);
+  };
+
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(
+      todoItemList.map((todoItem) => {
+        if (clickedTodoItem.id === todoItem.id) {
+          return {
+            id: clickedTodoItem.id,
+            todoItemContent: clickedTodoItem.todoItemContent,
+            isFinished: !clickedTodoItem.isFinished,
+          };
+        } else {
+          return todoItem;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <TodoItemInputField onSubmit={() => {}} />
-      <TodoItemList todoItemList={[]} />
-      <TodoItemList todoItemList={todoItemList} />
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+      />
     </div>
   );
 }
