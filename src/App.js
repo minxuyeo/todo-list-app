6ee0d26f0e0d5cdafdaeae3d398c67c37fdf1c85
여민서 <yeominseo@yeominseoui-MacBookAir.local>
2024-09-01 22:23:49 +0900

    [3/n] Create TodoItemInputField: Add Button to TodoItemInputField component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const TodoItemInputField = (props) => {
-  return <div> minseo</div>;
+  const [input, setInput] = useState("");
+  console.log(input);
+  return (
+    <div>
+      <TextField
+        id="todo-item-input"
+        label="Todo Item"
+        variant="outlined"
+        //유저가 입력한 값 업데이트 시키기
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <Button variant="outlined">Submit</Button>
+    </div>
+  );
 };
 function App() {
   return (
