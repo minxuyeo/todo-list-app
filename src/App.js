ad4537bbc4af04de9acbc22b94327730b2e1fe03
여민서 <yeominseo@yeominseoui-MacBookAir.local>
2024-09-01 21:39:17 +0900

    [1/n] Create TodoItemInputField: Create a placeholder react component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+const TodoItemInputField = (props) => {
+  return <div> minseo</div>;
+};
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoItemInputField />
+    </div>
+  );
 }
 
 export default App;
